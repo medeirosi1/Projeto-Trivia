304fc68d19b150321a1fed7478106b4e268350fe
Isabella Chagas <bellamarchagas@gmail.com>
2022-04-07 17:19:01 -0300

    feat: adiciona botão de jogar novamente

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  handleRouteBtnClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
         <Header />
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          onClick={ this.handleRouteBtnClick }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default Feedback;
