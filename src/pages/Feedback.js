ae3a69afd42ccce2948df97725de86b135688a9b
romulo-rp29 <romul.ceuma@gmail.com>
2022-04-07 17:30:17 -0300

    feat: ranking button on feedback page

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
         <Header />
+        <button
+          data-testid="btn-ranking"
+          type="button"
+          onClick={ () => history.push('/ranking') }
+        >
+          Ver Ranking
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default Feedback;
